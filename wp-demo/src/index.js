a72fb0a4faa95c86435e48fa1de245b821cf7062
buoyantair <buoyantair@gmail.com>
2018-04-27 14:24:45 +0530

    Output Management

--- wp-demo/src/index.js
+++ wp-demo/src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
+  btn.innerHTML = 'Summon the printer!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 
---
+++ wp-demo/src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+  console.log('I got summoned from print.js');
+}
